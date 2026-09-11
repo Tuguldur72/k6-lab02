b0e3861c3e4db5eb16cc34da9bf3cd7ce9f90518
Tuguldur72 <ttuk0650@gmail.com>
2026-09-11 22:35:46 +0800

    Step 4: Added thresholds and checks to script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-export const options = { 
-  vus: 5, 
-  duration: '30s' 
+export const options = {
+  vus: 30,
+  duration: '1m',
+  thresholds: {
+    // Нийт хүсэлтийн 95% нь 500ms-ээс хурдан байх ёстой
+    'http_req_duration': ['p(95)<500'],
+    // Алдааны хувь 1%-иас бага байх ёстой
+    'http_req_failed': ['rate<0.01'],
+  },
 };
 
 export default function () {
   const res = http.get('https://test.k6.io');
-  check(res, { 'status 200 байна': (r) => r.status === 200 });
+  
+  // Status 200 эсэхийг шалгах Check
+  check(res, {
+    'status 200 байна': (r) => r.status === 200,
+  });
+
   sleep(1);
 }
